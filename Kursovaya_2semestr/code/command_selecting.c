f897b2230eb7585584860e05b6650771b7831fc1
NikolaenkoKonstantin <62340704+NikolaenkoKonstantin@users.noreply.github.com>
2020-05-23 12:27:55 +0300

    Update command_selecting.c

--- Kursovaya_2semestr/code/command_selecting.c
+++ Kursovaya_2semestr/code/command_selecting.c
@@ -1,14 +1,13 @@
 #include "h_files/command_selecting.h"
 
-//Selecting a command from the menu
 void command_selecting(head *q)
 {
     char s[100];
-    int f = 8;
+    int f = 9;
 
-    while(f != 7)
+    while(f != 8)
     {
-        printf("Select a command:\n0) Reference\n1) Add\n2) Delete by search\n3) Database\n4) Search\n5) Edit card\n6) Sort cards\n7) Exit\n\nCommand:");
+        printf("Select a command:\n0) Reference\n1) Add\n2) Delete by search\n3) database\n4) Search\n5) Edit card\n6) Sort cards\n7) Save card index\n8) Exit\n\nCommand:");
         scanf("%s", s);
         f = charToInt(s[0]);
 
@@ -62,6 +61,17 @@ void command_selecting(head *q)
                 edit_card(q);
             else if(f == 6)
                 sort_cards(q);
+            else if(f == 7)
+            {
+                FILE *filewrite = fopen("data_base.csv", "w");
+
+                save_data(q, filewrite);
+
+                if (fclose(filewrite) == EOF)
+                    printf("Closing error code: %d\n", errno);
+                else
+                    printf("Closing OK\n");
+            }
         }
     }
 }

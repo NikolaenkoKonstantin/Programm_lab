f03f9218083585121128665f030540867eeb62f2
NikolaenkoKonstantin <62340704+NikolaenkoKonstantin@users.noreply.github.com>
2020-05-24 23:42:53 +0300

    Add files via upload

---
+++ Kursovaya_2semestr/code/h_files/option.h
@@ -0,0 +1,37 @@
+#ifndef OPTION_H_INCLUDED
+#define OPTION_H_INCLUDED
+#include "struct.h"
+#include <malloc.h>
+#include <stdlib.h>
+#include "io.h"
+
+void free_head(head *q);
+
+void free_node(node *temp);
+
+void free_list(head *q);
+
+void malloc_node(node *temp);
+
+head *create_head();
+
+node *create_node(head *q);
+
+void add_last(head *q);
+
+node *add_first(head *q);
+
+void delete_first(head *q);
+
+void delete_node(node *temp, head *q);
+
+node *copy_node(node *p, node *temp);
+
+void swap_node(node *p, node *temp, head *q);
+
+void transfer_node(node *p, node *temp, head *q);
+
+void copy_list(head *q1, head *q);
+
+
+#endif // OPTION_H_INCLUDED
---
+++ Kursovaya_2semestr/code/h_files/sort.h
@@ -0,0 +1,23 @@
+#ifndef SORT_H_INCLUDED
+#define SORT_H_INCLUDED
+#include <stdlib.h>
+#include "struct.h"
+#include "io.h"
+#include "option_With_List.h"
+
+void sortMega(head *q, int n);
+
+void sort_names(head *q);
+
+void sort_country(head *q);
+
+void sort_probability(head *q);
+
+void sort_wins(head *q);
+
+void sort_draws(head *q);
+
+void sort_cards(head *q);
+
+
+#endif // SORT_H_INCLUDED

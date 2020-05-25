abee2a236ab3913c7861beb2af7c8d989015d21c
NikolaenkoKonstantin <62340704+NikolaenkoKonstantin@users.noreply.github.com>
2020-05-25 14:45:33 +0300

    Add files via upload

---
+++ Kursovaya_2semestr/code/h_files/edit.h
@@ -0,0 +1,13 @@
+#ifndef EDIT_H_INCLUDED
+#define EDIT_H_INCLUDED
+#include <stdlib.h>
+#include "io.h"
+#include "struct.h"
+
+void edit_draws(node *temp);
+
+void edit_wins(node *temp);
+
+void edit_card(head *q);
+
+#endif // EDIT_H_INCLUDED
